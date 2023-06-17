20b751e1e603cbb01765bb07ced933156e4fe79b
astha148 <asthabharadwaj148@gmail.com>
2023-06-17 21:23:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+function WordCount() {
+  const [wordCount, setWordCount]=useState(0);
+  const [text, setText] = useState('');
+  const words = text.trim().split(/\s+/).length;
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+    setWordCount(words);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="mt-5 bg-primary-subtle border border-5 border-success container p-3">
+      <center>
+        <h1>React Task-1</h1><br/>
+        <h3>Responsive Paragraph Word Counter</h3>
+        <textarea rows={10} cols={50} value={text} onChange={handleTextChange}/>
+        <p>Word Count: {wordCount}</p>
+      </center>
     </div>
   );
 }
 
-export default App;
+export default WordCount;
